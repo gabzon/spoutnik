54c30d55d268c4ea13385828d1283f56713586a5
gabzon <gab.zambrano@gmail.com>
2016-03-22 19:54:14 +0100

    archive cycle bug commented

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 <?php endif; ?>
 
 
-<div class="ui container">    
+<div class="ui container">
     <div class="ui four column grid stackable">
         <?php while (have_posts()) : the_post(); ?>
             <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
         <?php endwhile; ?>
     </div>
+    <?php the_posts_navigation(); ?>
+    <br>
 </div>
-
-
-<?php the_posts_navigation(); ?>
+<br>
--- templates/archives/cycles.php
+++ templates/archives/cycles.php
@@ -20,30 +20,30 @@ foreach( $cats as $cat ) {
         $taxonomy_name = 'cycle';
         $termchildren = get_term_children( $term_id, $taxonomy_name );
 
-        $key = array_search('218',$termchildren);
-        $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
-        echo '<h3>Cycles mensuels</h3>';
-        echo '<ul class="archives-links">';
-        echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
-
-        $key = array_search('164',$termchildren);
-        $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
-
-        echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
-
-        $key = array_search('208',$termchildren);
-        $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
-
-        echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
-        echo '</ul>';
-        echo '<h3>Cycles ponctuels</h3>';
+        // $key = array_search('218',$termchildren);
+        // $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
+        // echo '<h3>Cycles mensuels</h3>';
+        // echo '<ul class="archives-links">';
+        // echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
+        //
+        // $key = array_search('164',$termchildren);
+        // $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
+        //
+        // echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
+        //
+        // $key = array_search('208',$termchildren);
+        // $term = get_term_by( 'id', $termchildren[$key], $taxonomy_name );
+        //
+        // echo '<li><a href="' . get_term_link( $termchildren[$key], $taxonomy_name ) . '">' . $term->name . '</a></li>';
+        // echo '</ul>';
+        //echo '<h3>Cycles ponctuels</h3>';
         echo '<ul class="archives-links">';
 
         foreach ( $termchildren as $child ) {
-            if (!($child == '218')) {
+            //if (!($child == '218')) {
                 $term = get_term_by( 'id', $child, $taxonomy_name );
             	echo '<li><a href="' . get_term_link( $child, $taxonomy_name ) . '">' . $term->name . '</a></li>';
-            }
+            //}
         }
         echo '</ul>';
     }
