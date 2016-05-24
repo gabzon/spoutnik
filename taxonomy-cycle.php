10a08efc1bdaccda5fc1e9587ace4b1965886b69
gabzon <gab.zambrano@gmail.com>
2016-05-24 19:55:07 +0200

    custom taxonomy template added for country, cycle, director

--- taxonomy-cycle.php
+++ taxonomy-cycle.php
@@ -8,6 +8,24 @@
 <?php endif; ?>
 
 <div class="ui container">
+
+    <?php $image_cycle = get_term_meta(get_queried_object()->term_id, 'image_cycle', false); ?>
+    <h1><?= get_queried_object()->name; ?></h1>
+    <div class="ui grid stackable">
+        <div class="one wide column"></div>
+        <div class="seven wide column">
+            <img src="<?= $image_cycle[0]; ?>" alt="" class="ui image" />
+        </div>
+        <div class="seven wide column">
+            <?php echo term_description( $term_id, $taxonomy ) ?>
+        </div>
+        <div class="one wide column"></div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<div class="ui container">
     <div class="ui four column grid stackable">
         <?php while (have_posts()) : the_post(); ?>
             <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
