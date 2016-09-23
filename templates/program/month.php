755416a0a3c6150162b8e50af5cd3a330b7857ea
gabzon <gab.zambrano@gmail.com>
2016-09-23 19:34:01 +0200

    program du mois 2

--- templates/program/month.php
+++ templates/program/month.php
@@ -34,8 +34,11 @@ $current_month = date('m');
           //echo date('M d, Y', $date);
 
           ?>
+
           <div class="column">
             <?php
+            $displayed_name = '';
+            $current_name = '';
             foreach ($horaire as $key) {
               $movie_projection_date = $key['film_date'];
               $movie_date_reformatted = DateTime::createFromFormat('d/m/Y', $movie_projection_date)->format('Y-m-d');
@@ -45,16 +48,17 @@ $current_month = date('m');
               $movie_projection_week = $movie_date->format("W");
               if ( $movie_projection_year == $current_year ) {
                 if ($movie_projection_month == $current_month) {
+                  $current_name = $movie_title;
                     ?>
-                    <h3>
-                      <a href="<?php echo the_permalink(); ?>" style="color:black">
-                        <?php echo $movie_title; ?>
-                      </a>
-                    </h3>
-                    <!-- <h5 class="h5-regular">
-                    <?php //echo __('Duration: ','sage') . get_post_meta($post->ID,'film_duration',true) ?>
-                  </h5> -->
-                  <br>
+                    <?php if ($displayed_name != $current_name): ?>
+                      <h3>
+                        <a href="<?php echo the_permalink(); ?>" style="color:black">
+                          <?php echo $movie_title; ?>
+                        </a>
+                      </h3>
+                    <br>
+                    <?php $displayed_name = $current_name; ?>
+                    <?php endif; ?>
                   <?php
                 setlocale(LC_TIME, "fr_FR");
                 echo '<h5 class="h5-regular">'. utf8_encode(strftime("%a %e %b", $movie_date->getTimestamp())) . ', ' . $horaire['film_heure'][$i] . '</h5>';
