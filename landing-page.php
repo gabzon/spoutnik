6fe5095404c02ab5d5c5346ef88a874ca9987bda
gabzon <gab.zambrano@gmail.com>
2016-10-01 17:01:00 +0200

    landing

--- landing-page.php
+++ landing-page.php
@@ -8,60 +8,87 @@ $full_program = $theme_options['spoutnik_programme_complet'];
 ?>
 <br>
 <div class="landing-content">
-    <div class="gray-shadow">
-        <div class="ui stackable grid" id="land">
-            <div class="one wide column"></div>
-            <div class="seven wide column">
-                <br>
-                <?php setlocale(LC_TIME, "fr_FR"); ?>
-                <!-- <h1><?php //echo Date('l jS F Y'); ?></h1> -->
-                <h1 style="text-transform:uppercase">
-                    <?php _e('TODAY ','sage'); ?><br>
-                    <?php echo  utf8_encode(strftime("%A %e %B"));   ?>
-                </h1>
-                <br>
-            </div>
-        </div>
-        <?php get_template_part('templates/today-films'); ?>
+  <div class="gray-shadow">
+    <div class="ui stackable grid" id="land">
+      <div class="one wide column"></div>
+      <div class="seven wide column">
+        <br>
+        <?php setlocale(LC_TIME, "fr_FR"); ?>
+        <!-- <h1><?php //echo Date('l jS F Y'); ?></h1> -->
+        <h1 style="text-transform:uppercase">
+          <?php _e('TODAY ','sage'); ?><br>
+          <?php echo  utf8_encode(strftime("%A %e %B"));   ?>
+        </h1>
+        <br>
+      </div>
+    </div>
+    <?php get_template_part('templates/today-films'); ?>
 
-        <?php get_template_part('templates/program/week'); ?>
-        <div class="ui one column grid">
-            <div class="column center aligned">
-                <?php //$page = get_page_by_title( 'Programme' ); ?>
-                <?php if ($full_program): ?>
-                    <a href="<?php echo esc_url( $full_program ); ?>" style="color:black">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/programme_complet.svg" alt="" />
-                        <br>
-                        <?php _e('full program','sage'); ?>
-                    </a>
-                <?php endif; ?>
-            </div>
-        </div>
-        <br><br>
+    <?php get_template_part('templates/program/week'); ?>
+    <div class="ui one column grid">
+      <div class="column center aligned">
+        <?php //$page = get_page_by_title( 'Programme' ); ?>
+        <?php if ($full_program): ?>
+          <a href="<?php echo esc_url( $full_program ); ?>" style="color:black">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/programme_complet.svg" alt="" />
+            <br>
+            <?php _e('full program','sage'); ?>
+          </a>
+        <?php endif; ?>
+      </div>
     </div>
-    <br>
-    <br>
-    <div class="ui grid stackable">
-        <div class="one wide column"></div>
-        <div class="fifteen wide column">
-            <h1 style="text-transform:uppercase"><?php _e('Also at Spoutnik','sage'); ?></h1>
-        </div>
+    <br><br>
+  </div>
+  <br>
+  <br>
+  <div class="ui grid stackable">
+    <div class="one wide column"></div>
+    <div class="fifteen wide column">
+      <h1 style="text-transform:uppercase"><?php _e('Also at Spoutnik','sage'); ?></h1>
     </div>
-    <br><br><br>
-    <?php
-    $the_slug = 'front-page';
-    $args = array(
-        'name'        => $the_slug,
-        'post_type'   => 'page',
-        'post_status' => 'publish',
-    );
-    $my_posts = get_posts($args);
-    if( $my_posts ) :
-        echo $my_posts[0]->post_content;
-    endif;
-    ?>
-    <br>
-    <br>
-    <br>
+  </div>
+  <br><br><br>
+
+  <?php
+  $args = array(
+    'posts_per_page'   => 3,
+    'category_name'    => 'focus',
+    'order'            => 'DESC',
+    'post_type'        => 'post',
+    'post_status'      => 'publish',
+    'suppress_filters' => true
+  );
+  $posts = get_posts( $args ); ?>
+  <div class="ui grid four column stackable">
+    <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+      <div class="one wide column"></div>
+      <div class="nine wide column">
+        <h1><a href="<?php the_permalink(); ?>" style="color:black; text-transform:uppercase;"><?php the_title(); ?></a></h1>
+        <?php the_excerpt(); ?>
+      </div>
+      <div class="five wide column">
+        <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'full'); ?>
+        <img src="<?= $image[0]; ?>" alt="" />
+      </div>
+      <div class="one wide column"></div>      
+    <?php endforeach;
+    wp_reset_postdata();?>
+  </div>
+
+  <?php
+  $the_slug = 'front-page';
+  $args = array(
+    'name'        => $the_slug,
+    'post_type'   => 'page',
+    'post_status' => 'publish',
+  );
+  $my_posts = get_posts($args);
+  if( $my_posts ) :
+    echo $my_posts[0]->post_content;
+  endif;
+  ?>
+  <br>
+  <br>
+  <br>
 </div>
 <br>
